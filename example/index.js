7414e5614789a88f3232bc3c36f1b3dc3d5fb7ea
vhtml <leaf_city@163.com>
2018-03-07 17:02:29 +0800

    添加对dpx的处理，用于解决1px border问题的一种替代方案

--- example/index.js
+++ example/index.js
@@ -1,14 +1,15 @@
 const fs = require('fs')
+const path = require('path')
 const postcss = require('postcss')
 const pxToViewport = require('..')
-const css = fs.readFileSync('main.css', 'utf8')
+const css = fs.readFileSync(path.resolve(__dirname, 'main.css'), 'utf8')
 const options = {
   replace: false
 }
 
 const processedCss = postcss(pxToViewport(options)).process(css).css
 
-fs.writeFile('main-viewport.css', processedCss, function (err) {
+fs.writeFile(path.resolve(__dirname, 'main-viewport.css'), processedCss, function (err) {
   if (err) {
     throw err
   }
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const objectAssign = require('object-assign')
 // Any digit followed by px
 // !singlequotes|!doublequotes|!url()|pixelunit
 const pxRegex = /"[^"]+"|'[^']+'|url\([^)]+\)|(\d*\.?\d+)px/ig
+const dpxRegExp = /(\d*\.?\d+)dpx/ig
 
 const defaults = {
   viewportWidth: 750,
@@ -15,18 +16,18 @@ const defaults = {
   unitPrecision: 5,
   selectorBlackList: [],
   minPixelValue: 1,
-  px2rem: true,
-  px2vw: true,
+  toRem: true,
   mediaQuery: false
 }
 
-module.exports = postcss.plugin('postcss-plugin-px2viewport', function (options) {
+module.exports = postcss.plugin('postcss-plugin-px2viewport', options => {
   const opts = objectAssign({}, defaults, options)
   const px2vwReplace = createPx2Viewport(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision)
   const px2remReplace = createPx2Rem(opts.viewportWidth, opts.remRatio, opts.minPixelValue, opts.unitPrecision)
+  const dpxReplace = createDpx(opts.unitPrecision)
 
-  return function (css) {
-    css.walkDecls(function (decl, i) {
+  return css => {
+    css.walkDecls((decl, i) => {
       if (decl.value.indexOf('px') === -1) return
 
       if (blacklistedSelector(opts.selectorBlackList, decl.parent.selector)) return
@@ -34,11 +35,30 @@ module.exports = postcss.plugin('postcss-plugin-px2viewport', function (options)
       const declValue = decl.value
 
       decl.value = declValue.replace(pxRegex, px2vwReplace)
-      opts.px2rem && decl.parent.insertBefore(decl, decl.clone({value: declValue.replace(pxRegex, px2remReplace)}))
+      if (opts.toRem) {
+        const cloned = decl.clone({value: declValue.replace(pxRegex, px2remReplace)})
+        if (cloned.value === declValue) return
+        decl.parent.insertBefore(decl, cloned)
+      }
+    })
+
+    css.walkRules(rule => {
+      const newRule = postcss.rule()
+      newRule.selector = '.hairlines ' + rule.selector
+      rule.each(decl => {
+        if (dpxRegExp.test(decl.value)) {
+          newRule.nodes.push(decl.clone({value: decl.value.replace(dpxRegExp, dpxReplace)}))
+          decl.value = decl.value.replace('d', '')
+        }
+      })
+
+      if (newRule.nodes.length) {
+        rule.parent.insertAfter(rule, newRule)
+      }
     })
 
     if (opts.mediaQuery) {
-      css.walkAtRules('media', function (rule) {
+      css.walkAtRules('media', rule => {
         if (rule.params.indexOf('px') === -1) return
         rule.params = rule.params.replace(pxRegex, px2vwReplace)
       })
@@ -47,7 +67,7 @@ module.exports = postcss.plugin('postcss-plugin-px2viewport', function (options)
 })
 
 function createPx2Viewport (viewportSize, minPixelValue, unitPrecision) {
-  return function (m, $1) {
+  return (m, $1) => {
     if (!$1) return m
 
     let pixels = parseFloat($1)
@@ -60,13 +80,22 @@ function createPx2Viewport (viewportSize, minPixelValue, unitPrecision) {
 }
 
 function createPx2Rem (viewportSize, remRatio, minPixelValue, unitPrecision) {
-  return function (m, $1) {
+  return (m, $1) => {
     if (!$1) return m
 
     let pixels = parseFloat($1)
     if (pixels <= minPixelValue) return m
 
     return toFixed((pixels / viewportSize * remRatio), unitPrecision) + 'rem'
+  }
+}
+
+function createDpx (unitPrecision) {
+  return (m, $1) => {
+    if (!$1) return m
+
+    let pixels = parseFloat($1)
+    return toFixed(pixels / 2, unitPrecision) + 'px'
   }
 }
 
@@ -78,7 +107,7 @@ function toFixed (number, precision) {
 
 function blacklistedSelector (blacklist, selector) {
   if (typeof selector !== 'string') return
-  return blacklist.some(function (regex) {
+  return blacklist.some(regex => {
     if (typeof regex === 'string') return selector.indexOf(regex) !== -1
     return selector.match(regex)
   })
